18aa92395fabcaa85d25f061be043ca1a75763a5
konatsup <konatsukomatsu@gmail.com>
2019-02-19 04:36:47 +0900

    Fix User.name validation to loose

--- models.rb
+++ models.rb
@@ -9,8 +9,7 @@ end
 class User < ActiveRecord::Base
   has_secure_password
   validates :name,
-    presence: true,
-    format: { with: /\A\w+\z/ }
+    presence: true
   validates :password,
     length: {in: 5..10}
   has_many :posts
