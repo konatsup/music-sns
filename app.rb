7dd63fc62eed672bf0cd70de62781d30ce227002
konatsup <konatsukomatsu@gmail.com>
2019-02-19 01:34:35 +0900

    Create pages and routing

--- app.rb
+++ app.rb
@@ -1,3 +1,23 @@
 require 'bundler/setup'
 Bundler.require
 require 'sinatra/reloader' if development?
+
+get '/' do
+  erb :index
+end
+
+get '/sign_up' do
+  erb :signup
+end
+
+get '/search' do
+  erb :search
+end
+
+get '/home' do
+  erb :home
+end
+
+get '/edit/:id' do
+  erb :edit
+end
